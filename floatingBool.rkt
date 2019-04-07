69e404af1e47d97cfd49ad6f31bef548b60383e6
Torstein Nordgård-Hansen <torsteinnh@gmail.com>
2019-04-07 03:39:25 +0200

    More stuff has been added in racket

--- floatingBool.rkt
+++ floatingBool.rkt
@@ -1,18 +1,57 @@
-;;; Small file that defines boolean algebra defined around borders in floatin-points in racket
+;;; Small file that defines boolean algebra defined around borders in floating-points in racket
 
 #lang racket
 
 (provide bt bf
-         float-or float-and)
+         ; Many functions should be added here sometime
+         )
+
 
 
 ;; True and false, this is mostly bekause 0.0 and -0.0 is hard to type
+
 (define bt 0.0)
 (define bf -0.0)
 
 
-(define (float-or a b)
+
+;; Primitives
+
+(define (f-or a b)
   (+ a b))
 
-(define (float-and a b)
+(define (f-not a)
+  (- a))
+
+(define (f-xnor a b)
+  (* a b))
+
+
+
+;; Simple komposittes shortened
+
+(define (f-and a b)
   (* a b (+ a b)))
+
+(define (f-nand a b)
+  (- (* a b (+ a b))))
+ 
+(define (f-xor a b)
+  (- (* a b)))
+
+(define (f-a=>b a b)
+  (- b a))
+
+(define f-a<=>b f-xnor)
+
+
+
+;; More advanced funny stuff
+
+(define (f-fadder a b c)
+  ; Takes inn a, b and carry inn, returns out and carry out in a cons
+  (let ((out (* a b c))
+        (carry (* (+ a b) (+ c (* a b)))))
+    (cons out carry))
+  )
+
